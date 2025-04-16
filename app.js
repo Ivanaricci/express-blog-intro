f3ce1f0e45546d73ee071ded9c5e24cbe661a98c
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-16 16:57:57 +0200

    [UPD] Done app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+// importazione di express
+const express = require('express');
+
+// inizializzazione di express nella variabile app
+const app = express();
+
+// definizione del numero di porta
+const port = 3000;
+
+// definizione della rotta base
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+// inserimento del metodo che lascia in ascolto il server
+app.listen(port, () => {
+    console.log(`server in ascolto`)
+})
